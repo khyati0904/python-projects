53438c79d04940627c1e49d4a5cf269f0b990a78
khyati0904 <156738402+khyati0904@users.noreply.github.com>
2024-06-06 14:15:42 +0530

    Update passwordgeneator.py

--- passwordgeneator.py
+++ passwordgeneator.py
@@ -3,8 +3,8 @@
 import string
 import random
 
-length= int(input("Enter length of password="))
-a= string.ascii_letters+ string.digits +string.punctuation
+length= int(input("Enter length of password="))   #it will take input from user
+a= string.ascii_letters+ string.digits +string.punctuation  #allows user to generate password with letters, digits, special characters
 res = random.choices(a,k=length)
 password= ''.join(res)
 print(password)
